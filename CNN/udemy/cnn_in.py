ce1ac9e8f2daeedfde5ddc99bc1d7ce3ca0d48fd
ChinchillasTaraToro <30887754+geniuschinchillas@users.noreply.github.com>
2019-09-09 02:04:54 -0600

    cnn

--- CNN/udemy/cnn_in.py
+++ CNN/udemy/cnn_in.py
@@ -34,12 +34,12 @@ train_datagen = ImageDataGenerator(rescale=1./255,
 
 test_datagen = ImageDataGenerator(rescale=1./255)
 
-training_set = train_datagen.flow_from_directory('dataset/training_set',
+training_set = train_datagen.flow_from_directory("C:/Users/garli/Labs/ChinchillasSnack/CNN/udemy/dataset/training_set",
                                                  target_size = (64, 64),
                                                  batch_size = 32,
                                                  class_mode = 'binary')
 
-test_set = test_datagen.flow_from_directory('dataset/test_set',
+test_set = test_datagen.flow_from_directory("C:/Users/garli/Labs/ChinchillasSnack/CNN/udemy/dataset/test_set",
                                                         target_size=(64, 64),
                                                         batch_size=32,
                                                         class_mode='binary')
@@ -49,3 +49,4 @@ classifier.fit_generator(training_set,
                          nb_epoch = 25,
                          validation_data = test_set,
                          nb_val_samples = 2000)
+
